a1741c117ce5b56b779044a0aa291bd3ea1648ba
rekayamontadhar <montadar.rekaya-extern@renault.com>
2021-11-29 17:14:14 +0100

    updates stg/dim/tests

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,24 +1,9 @@
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial.jaffle_shop.customers`
-
+    select * from {{ ref('stg_customers')}}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial.jaffle_shop.orders`
-
+    select * from {{ ref('fct_orders')}}
 ),
 
 customer_orders as (
---
+++ models/staging/jaffle_shop/src_jaffle_shop.yml
@@ -0,0 +1,8 @@
+version: 2
+
+sources:
+  - name: jaffle_shop
+    database: dbt-tutorial
+    tables:
+      - name: customers
+      - name: orders
